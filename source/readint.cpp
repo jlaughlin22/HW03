10ec3828f6edfeda869e75bab2d2cc06c7c7c867
jlaughlin22 <46356607+jlaughlin22@users.noreply.github.com>
2019-06-03 23:28:22 -0700

    Added doc few comments and removed test file

--- source/readint.cpp
+++ source/readint.cpp
@@ -9,32 +9,26 @@ using namespace std;
 
 int read_int(const string &prompt, int low, int high)
 {
-    if( high <= low ){
+    if( high <= low ){//invalid low/high values 
         throw invalid_argument("Invalid values have been entered.");
     }
     int input;
-    while (true) {
-             
+    while (true) {//keep gathering inputs until a good one is entered
         try {
             cout << prompt; //<< endl;
             cin >> input;
-            if( !(input >= low && input <= high) ){
+            if( !(input >= low && input <= high) ){//if outside rang throw range_error
                 throw range_error("The entered value is outside the bounds of the range.");
             }
             return input;
         }
-        catch (range_error& m) {
+        catch (range_error& m) {//catch range_error
             cout << "The entered value is outside the bounds of the range.\n";
         }
-        catch(ios_base::failure& ex) {
+        catch(ios_base::failure& ex) {//catch failure
             cout << "Error1\n";
             cin.clear();
             cin.ignore(numeric_limits<int>::max(), '\n'); 
         }
-        catch(invalid_argument& t) {
-
-        }
     }
-    
-    
 }
